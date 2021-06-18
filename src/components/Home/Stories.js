981a2bc7d9bdca6fa94138bd504c94fee3083702
Mehmet Sağır <62952843+mehmetsagir@users.noreply.github.com>
2021-06-19 01:13:37 +0300

    Instagram Clone
    Please enter the commit message for your changes. Lines starting

--- src/components/Home/Stories.js
+++ src/components/Home/Stories.js
@@ -1,66 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import styles from '@modules/Home/Stories.module.scss'
 import Avatar from '@components/Avatar'
 
 const Stories = () => {
- 
+  const [isDown, setIsDown] = useState(false)
+  const [scrollLeft, setScrollLeft] = useState(0) 
+  const [scrollX, setScrollX] = useState(0) 
+  
+  function mouseUp(e) {
+    setIsDown(false)
+  }
+
+  function mouseLeave(e) {
+    setIsDown(false)
+  }
+
+  function mouseDown(e) {
+    setIsDown(true)
+    setScrollX(e.pageX - document.getElementById('stories').offsetLeft)
+    setScrollLeft(document.getElementById('stories').scrollLeft)
+  }
+
+  function mouseMove(e) {
+    if (!isDown) return;
+    let element = e.pageX - document.getElementById('stories').offsetLeft;
+    let scrolling = (element - scrollX) * 2;
+    document.getElementById('stories').scrollLeft = scrollLeft - scrolling;
+  }
+
+  const userList = ["adelia","adelice","adelina","adelind","adeline","adella","adelle","adelynn","aden","adena","adeniyi","adey","adi","adiana","adie","adina","aditya","admin","adnan","adon","adonis","adora","adore","adoree","adorne","adrea","adreanna","adri","adria","adriaens"]
+
   return (
     <div className={`${styles.Container}`}>
-      <ul>
-        <li>
-          <Avatar story="true" />
-          <span>Username</span>
-        </li>
-        <li>
-          <Avatar story="true" />
-          <span>Username</span>
-        </li>
-        <li>
-          <Avatar story="true" />
-          <span>Username</span>
-        </li>
-        <li>
-          <Avatar story="true" />
-          <span>Username</span>
-        </li>
-        <li>
-          <Avatar story="true" />
-          <span>Username</span>
-        </li>
-        <li>
-          <Avatar story="true" />
-          <span>Username</span>
-        </li>
-        <li>
-          <Avatar story="true" />
-          <span>Username</span>
-        </li>
-        <li>
-          <Avatar story="true" />
-          <span>Username</span>
-        </li>
-        <li>
-          <Avatar story="true" />
-          <span>Username</span>
-        </li>
-        <li>
-          <Avatar story="true" />
-          <span>Username</span>
-        </li>
-        <li>
-          <Avatar story="true" />
-          <span>Username</span>
-        </li>
-        <li>
-          <Avatar story="true" />
-          <span>Username</span>
-        </li>
-        <li>
-          <Avatar story="true" />
-          <span>Username</span>
-        </li>
-      </ul>
+      <ul id="stories" onMouseDown={mouseDown} onMouseLeave={mouseLeave} onMouseUp={mouseUp} onMouseMove={mouseMove}>
+        {
+          userList.map(user => (
+            <li key={user}>
+              <Avatar story="true" />
+              <span>{ user }</span>
+            </li>
+          ))
+        }
+       </ul>
     </div>
   )
 }
