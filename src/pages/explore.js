0563038f8d2620628dd889a02b906a2bcb14a65f
Mehmet Sağır <62952843+mehmetsagir@users.noreply.github.com>
2021-06-19 05:27:44 +0300

    Added explore page

---
+++ src/pages/explore.js
@@ -0,0 +1,71 @@
+import React from 'react'
+
+import Layout from '@components/Layout'
+import Post from '@components/Explore/Post'
+
+import styles from '@modules/Explore/Post.module.scss'
+
+const explore = () => {
+
+  const fakePostData = [
+    {
+      id: 1,
+      like: 23,
+      comment: 1
+    },
+    {
+      id: 2,
+      like: 65,
+      comment: 5
+    },
+    {
+      id: 3,
+      like: 244,
+      comment: 3
+    },
+    {
+      id: 4,
+      like: 125,
+      comment: 7
+    },
+    {
+      id: 5,
+      like: 986,
+      comment: 25
+    },
+    {
+      id: 6,
+      like: 757,
+      comment: 42
+    },
+    {
+      id: 7,
+      like: 7,
+      comment: 1
+    },
+    {
+      id: 8,
+      like: 34,
+      comment: 6
+    },
+    {
+      id: 9,
+      like: 64,
+      comment: 2
+    },
+  ]
+
+  return (
+    <Layout>
+      <div className={styles.Posts}>
+        {
+          fakePostData.map(post => (
+            <Post key={post.id} post={post} />
+          ))
+        }
+      </div>
+    </Layout>
+  )
+}
+
+export default explore
